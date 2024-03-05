be91d1119c3f066fbdf9b37b68bc03814413db68
Farzad Meow Khalafi <farzadk@gmail.com>
2024-03-05 11:27:27 -0500

    format fix Branch name: master

--- index.ts
+++ index.ts
@@ -1,88 +1,90 @@
 export class Query {
-    private param_select : string[] = [];
-    private param_table: string = '';
-    private param_where: any[] = [];
+  param_select: string[] = [];
+  param_table: string = "";
+  param_where: any[] = [];
 
-    constructor() {
+  constructor() {}
 
+  public select(selects: string | string[]) {
+    if (typeof selects === "string") {
+      let selects2 = selects;
+      let matches = selects.match(
+        /"?(?<alias>[a-zA-Z0-9]*)"?\."?(?<var_name>.*)"?/,
+      );
+      if (matches) {
+        selects2 =
+          '"' + matches.groups?.alias + '".' + matches.groups?.var_name;
+      }
+
+      this.param_select.push(selects2);
+    } else if (Array.isArray(selects)) {
+      selects.map((a) => {
+        this.select(a);
+      });
     }
 
-    public select(selects : string | string[]) {
-        if (typeof selects === 'string')
-        {
-            let selects2 = selects;
-            let matches = selects.match(/"?(?<alias>[a-zA-Z0-9]*)"?\."?(?<var_name>.*)"?/);
-            if(matches)
-            {
-                selects2 = "\""+matches.groups?.alias+"\"." + matches.groups?.var_name;
-            }
+    return this;
+  }
 
-            this.param_select.push(selects2);
-        }
-        else if(Array.isArray(selects))
-        {
-            selects.map((a) => {this.select(a);});
-        }
+  public from(table: string) {
+    this.param_table = table;
 
-        return this;
+    return this;
+  }
+
+  public where(
+    column: string,
+    value: any,
+    options: object = { condition: "=" },
+  ) {
+    this.param_where.push({
+      column_name: column,
+      value: value,
+      options: options,
+    });
+
+    return this;
+  }
+
+  public toFullSQL() {
+    let rc: string[] = [];
+    if (this.param_select.length > 0) {
+      rc.push(
+        "SELECT " +
+          this.param_select.reduce((a, b) => {
+            return a + ", " + b;
+          }),
+      );
     }
 
-    public from(table: string) {
-        this.param_table = table;
-
-        return this;
+    if (this.param_table) {
+      rc.push("FROM " + this.param_table);
     }
 
-    public where(column: string,value: any, options: object = {condition: '='}) {
-        this.param_where.push({column_name: column, value: value, options: options})
+    if (this.param_where.length) {
+      rc.push("WHERE");
+      this.param_where.map((w) => {
+        let cond = "=";
+        let value = w.value;
+        if (w.options?.condition) {
+          cond = w.options.condition;
+        }
 
-        return this;
+        if (typeof value == "string") {
+          value = "'" + value + "'";
+        } else if (typeof value == "number") {
+          value = value;
+        } else {
+        }
+        rc.push(w.column_name + " " + cond + " " + value);
+      });
     }
 
-    public toFullSQL() {
-        let rc : string[] = [];
-        if(this.param_select.length > 0)
-        {
-            rc.push('SELECT ' + this.param_select.reduce((a,b) => {
-                return a + ', ' + b; 
-            }));
-        }
+    let rc_str = "";
+    rc_str = rc.reduce((a, b) => {
+      return a + " " + b;
+    });
 
-        if(this.param_table) {
-            rc.push('FROM ' + this.param_table);
-        }
-
-        if(this.param_where.length) {
-            rc.push('WHERE');
-            this.param_where.map((w) => {
-                let cond = '=';
-                let value = w.value;
-                if(w.options?.condition)
-                {
-                    cond = w.options.condition;
-                }
-
-                if(typeof value == 'string')
-                {
-                    value = '\'' + value + '\'';
-                }
-                else if(typeof value == 'number')
-                {
-                    value = value;
-                }
-                else
-                {
-
-                }
-                rc.push(w.column_name + ' ' + cond + ' ' + value);
-            })
-        }
-
-        let rc_str = '';
-        rc_str = rc.reduce((a,b) => {
-            return a + ' ' + b;
-        })
-
-        return rc_str;
-    }
+    return rc_str;
+  }
 }
--- tests/basics.spec.ts
+++ tests/basics.spec.ts
@@ -1,94 +1,83 @@
-import {describe, expect, test} from '@jest/globals';
-import {Query} from '../index';
+import { describe, expect, test } from "@jest/globals";
+import { Query } from "../index";
 
-describe('basic select statements', () => {
-  test('basic select', () => {
+describe("basic select statements", () => {
+  test("basic select", () => {
     let query = new Query();
-    query.select('*')
-      .from('table');
-      
-    expect(query.toFullSQL()).toBe('SELECT * FROM table');
+    query.select("*").from("table");
+
+    expect(query.toFullSQL()).toBe("SELECT * FROM table");
   });
 
-  test('basic select with array', () => {
+  test("basic select with array", () => {
     let query = new Query();
-    query.select(['a','b','c'])
-      .from('table');
-      
-    expect(query.toFullSQL()).toBe('SELECT a, b, c FROM table');
+    query.select(["a", "b", "c"]).from("table");
+
+    expect(query.toFullSQL()).toBe("SELECT a, b, c FROM table");
   });
 
-  test('basic select with alias', () => {
+  test("basic select with alias", () => {
     let query = new Query();
-    query.select('t1.*')
-      .from('table t1');
-      
+    query.select("t1.*").from("table t1");
+
     expect(query.toFullSQL()).toBe('SELECT "t1".* FROM table t1');
   });
 
-  test('basic select from where', () => {
+  test("basic select from where", () => {
     let query = new Query();
-    query.select('*')
-      .from('table')
-      .where('col1','value', {condition: '='});
-      
-    expect(query.toFullSQL()).toBe('SELECT * FROM table WHERE col1 = \'value\'');
+    query.select("*").from("table").where("col1", "value", { condition: "=" });
+
+    expect(query.toFullSQL()).toBe("SELECT * FROM table WHERE col1 = 'value'");
 
     query = new Query();
-    query.select('*')
-      .from('table')
-      .where('col1','value');
-      
-    expect(query.toFullSQL()).toBe('SELECT * FROM table WHERE col1 = \'value\'');
+    query.select("*").from("table").where("col1", "value");
+
+    expect(query.toFullSQL()).toBe("SELECT * FROM table WHERE col1 = 'value'");
 
     query = new Query();
-    query.select('*')
-      .from('table')
-      .where('col1',1234.56);
-      
-    expect(query.toFullSQL()).toBe('SELECT * FROM table WHERE col1 = 1234.56');
+    query.select("*").from("table").where("col1", 1234.56);
+
+    expect(query.toFullSQL()).toBe("SELECT * FROM table WHERE col1 = 1234.56");
 
     query = new Query();
-    query.select('*')
-      .from('table')
-      .where('col1','value')
-      .Where('col2','value2');
-      
-    expect(query.toFullSQL()).toBe('SELECT * FROM table WHERE col1 = \'value\' AND col2 = \'value2\'');
+    query
+      .select("*")
+      .from("table")
+      .where("col1", "value")
+      .Where("col2", "value2");
+
+    expect(query.toFullSQL()).toBe(
+      "SELECT * FROM table WHERE col1 = 'value' AND col2 = 'value2'",
+    );
   });
 
-
-  test('basic select limit and offset', () => {
+  test("basic select limit and offset", () => {
     let query = new Query();
-    query.select('*')
-      .from('table')
-      .limit(100);
-      
-    expect(query.toFullSQL()).toBe('SELECT * FROM table LIMIT 100');
+    query.select("*").from("table").limit(100);
+
+    expect(query.toFullSQL()).toBe("SELECT * FROM table LIMIT 100");
 
     query = new Query();
-    query.select('*')
-      .from('table')
-      .offset(100);
-      
-    expect(query.toFullSQL()).toBe('SELECT * FROM table OFFSET 100');
+    query.select("*").from("table").offset(100);
+
+    expect(query.toFullSQL()).toBe("SELECT * FROM table OFFSET 100");
 
     query = new Query();
-    query.select('*')
-      .from('table')
-      .limit(111)
-      .offset(222);
-      
-    expect(query.toFullSQL()).toBe('SELECT * FROM table LIMIT 111 OFFSET 222');
+    query.select("*").from("table").limit(111).offset(222);
+
+    expect(query.toFullSQL()).toBe("SELECT * FROM table LIMIT 111 OFFSET 222");
   });
 
-  test('orWhere', () => {
+  test("orWhere", () => {
     let query = new Query();
-    query.select('*')
-      .from('table')
-      .where('col1','value')
-      .orWhere('col2','value2');
-      
-    expect(query.toFullSQL()).toBe('SELECT * FROM table WHERE col1 = \'value\' OR col2 = \'value2\'');
+    query
+      .select("*")
+      .from("table")
+      .where("col1", "value")
+      .orWhere("col2", "value2");
+
+    expect(query.toFullSQL()).toBe(
+      "SELECT * FROM table WHERE col1 = 'value' OR col2 = 'value2'",
+    );
   });
 });
