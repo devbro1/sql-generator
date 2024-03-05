cb9057a7b3b89d560831c36f3e2df21f25ce9b1f
Farzad Meow Khalafi <farzadk@gmail.com>
2024-03-05 11:46:49 -0500

    added where clause file Branch name: master

--- tests/where_clause.spec.ts
+++ tests/where_clause.spec.ts
@@ -2,25 +2,38 @@ import { describe, expect, test } from "@jest/globals";
 import { Query } from "../index";
 
 describe("where clause", () => {
-  test("basic select from where", () => {
+  test("where 1", () => {
     let query = new Query();
-    query.select("*").from("table").where("col1", "value", { condition: "=" });
+    query.select("*").from("table").where("col1", "=", "value");
 
     expect(query.toFullSQL()).toBe("SELECT * FROM table WHERE col1 = 'value'");
   });
-  test("basic select from where", () => {
+  test("where 2", () => {
     let query = new Query();
     query.select("*").from("table").where("col1", "value");
 
     expect(query.toFullSQL()).toBe("SELECT * FROM table WHERE col1 = 'value'");
   });
-  test("basic select from where", () => {
+  test("where 3", () => {
     let query = new Query();
     query.select("*").from("table").where("col1", 1234.56);
 
     expect(query.toFullSQL()).toBe("SELECT * FROM table WHERE col1 = 1234.56");
   });
-  test("basic select from where", () => {
+
+  test("where 3", () => {
+    let query = new Query();
+    query
+      .select("*")
+      .from("table")
+      .where([
+        ["status", "=", "1"],
+        ["subscribed", "<>", "1"],
+      ]);
+
+    expect(query.toFullSQL()).toBe("SELECT * FROM table WHERE col1 = 1234.56");
+  });
+  test("where 4", () => {
     let query = new Query();
     query
       .select("*")
